254f27811ea0369615eabc570408e436facd13a3
Iofthenight <ricardotferreira@gmail.com>
2019-11-08 00:04:21 +0000

    S2E17 - Ajustes no endpoint /Categoria{id}

--- src/main/java/com/iofhtenight/cursomc/services/CategoriaService.java
+++ src/main/java/com/iofhtenight/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.iofhtenight.cursomc.domain.Categoria;
 import com.iofhtenight.cursomc.repositories.CategoriaRepository;
+import com.iofhtenight.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() ->  new ObjectNotFoundException(
+				"Objecto não encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
