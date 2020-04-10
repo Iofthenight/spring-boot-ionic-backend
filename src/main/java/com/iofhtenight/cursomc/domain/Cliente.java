31ab577b055b09cf5ce7f7008893dc290e3ba5f6
Iofthenight <ricardotferreira@gmail.com>
2020-04-10 21:06:35 +0100

    S3E44 - Ajustando e testando a deleção de Cliente.

--- src/main/java/com/iofhtenight/cursomc/domain/Cliente.java
+++ src/main/java/com/iofhtenight/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -23,7 +24,7 @@ import com.iofhtenight.cursomc.domain.enums.TipoCliente;
 public class Cliente implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	@Id
+	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
@@ -31,7 +32,7 @@ public class Cliente implements Serializable{
 	private String CpfOuCnpj;
 	private Integer tipo;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
