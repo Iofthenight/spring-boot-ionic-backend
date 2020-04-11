cb570f171c95f8348fe95eda7f72a74a30857f05
Iofthenight <ricardotferreira@gmail.com>
2020-04-11 21:30:06 +0100

    S3E45 - Validação costumizada de CPF e CNPJ

---
+++ src/main/java/com/iofhtenight/cursomc/services/validation/ClientInsertValidator.java
@@ -0,0 +1,41 @@
+package com.iofhtenight.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.iofhtenight.cursomc.domain.enums.TipoCliente;
+import com.iofhtenight.cursomc.resources.exception.FieldMessage;
+import com.iofhtenight.cursomc.services.validation.utils.BR;
+import com.iofthenight.cursomc.dto.ClienteNewDTO;
+
+public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClientInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+// inclua os testes aqui, inserindo erros na lista
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOASFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
