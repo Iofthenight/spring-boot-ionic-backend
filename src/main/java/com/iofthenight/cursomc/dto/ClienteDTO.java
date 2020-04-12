1da014a97bfc2af2c0a826f6f041d706318d7ba6
Iofthenight <ricardotferreira@gmail.com>
2020-04-12 13:11:54 +0100

    S3E47 - Validação customizada: email não repetido na actualização do Cliente.

--- src/main/java/com/iofthenight/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/iofthenight/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.iofhtenight.cursomc.domain.Cliente;
+import com.iofhtenight.cursomc.services.validation.ClientUpdate;
 
+@ClientUpdate
 public class ClienteDTO  implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
